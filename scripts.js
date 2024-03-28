0814ea307a4e6cdecf234500cedf316da6f3f53e
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-29 00:09:24 +0100

    Added event listener and grid-like page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
-let numberCard = 16;
+let numberCard = 42;
 
 for (let index = 0; index < numberCard*numberCard; index++) {
     const divContainer = document.querySelector(".container");
     let divCard = document.createElement("div");
     divCard.classList.toggle("cards");
-    divCard.setAttribute("style","width:"+(1/numberCard)+"%;");
+    let rowWidth = (1/numberCard)*100;
+    divCard.setAttribute("style","width:"+rowWidth+"%;");
     divContainer.appendChild(divCard); 
 }
 
+const divContainer = document.querySelector(".container");
+
+divContainer.addEventListener("mouseover", (e)=>{
+    e.target.style.backgroundColor="yellow";
+})
