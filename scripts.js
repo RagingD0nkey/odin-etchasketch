3b2cf33eb80527adf7d0938ffabe04089de2b482
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-03-29 19:22:36 +0100

    Added opacity and rainbow switch

--- scripts.js
+++ scripts.js
@@ -1,16 +1,62 @@
-let numberCard = 42;
+let gridSize = 4;
 
-for (let index = 0; index < numberCard*numberCard; index++) {
-    const divContainer = document.querySelector(".container");
-    let divCard = document.createElement("div");
-    divCard.classList.toggle("cards");
-    let rowWidth = (1/numberCard)*100;
-    divCard.setAttribute("style","width:"+rowWidth+"%;");
-    divContainer.appendChild(divCard); 
+// Random number between 0 and Upper Limit
+function rndNumber(ULimit){
+    return Math.floor(Math.random() * ULimit);
 }
+
+
+//GRID Creation
+function drawGrid(gridSize) {
+    for (let index = 0; index < gridSize*gridSize; index++) {
+            const divContainer = document.querySelector(".container");
+            let divCard = document.createElement("div");
+            divCard.classList.toggle("cards");
+            let rowWidth = (1/gridSize)*100;
+            divCard.setAttribute("style","width:"+rowWidth+"%;");
+            divContainer.appendChild(divCard); 
+    }
+}
+
+//GRID Deletion
+function clearGrid() {
+    const divCardsAll = document.querySelectorAll(".cards");
+    for(let index=0;index<divCardsAll.length;index++){
+        divCardsAll[index].remove();
+    }
+}
+
+drawGrid(gridSize);
 
 const divContainer = document.querySelector(".container");
 
 divContainer.addEventListener("mouseover", (e)=>{
-    e.target.style.backgroundColor="yellow";
+
+    const rainbowCheck = document.querySelector("#rainbow");
+    const opacityCheck = document.querySelector("#opacity");
+
+    if(e.target.className==="cards") {
+        if (rainbowCheck.checked ===true) {
+            e.target.style.backgroundColor="rgb("+rndNumber(256)+" "+rndNumber(256)+" "+rndNumber(256)+")";
+        }
+        else {
+            e.target.style.backgroundColor="black";
+        }
+
+        if (opacityCheck.checked===true){
+            e.target.style.opacity -= -0.1; // since opacity is a string "0.1" it avoids concatenations via +
+        }
+        else {
+            e.target.style.opacity =1;
+        }
+    }
+})
+
+
+const btnResize = document.querySelector(".resize button");
+
+btnResize.addEventListener("click", () =>{
+    let gridSize = Number(prompt("Enter the size of the new grid - between 2 and 100"));
+    clearGrid();
+    drawGrid(gridSize);
 })
